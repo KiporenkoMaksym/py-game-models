a26fdb9f001c09570f745932f9b8100b33d63a5d
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-11-03 20:31:24 +0200

    cpmplete2

--- db/models.py
+++ db/models.py
@@ -1,31 +1,31 @@
 from django.db import models
-from django.db.models import ForeignKey
 
 
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    bonus = models.CharField(max_length=255, help_text="A short description of the bonus players can get.")
+    bonus = models.CharField(
+        max_length=255,
+        help_text="A short description of the bonus players can get.")
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
         related_name="skills"
     )
 
-
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
-
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
-    bio = models.CharField(max_length=255, help_text="A short description about yourself provided by the user.")
+    bio = models.CharField(
+        max_length=255,
+        help_text="A short description about yourself provided by the user.")
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
